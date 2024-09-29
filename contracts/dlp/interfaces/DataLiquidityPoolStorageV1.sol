56450fa9ac12d0690ff58b9bf7423fbd32419af3
guanxin <richqi186@gmail.com>
2024-09-29 22:30:11 +0800

    Add files via upload

---
+++ contracts/dlp/interfaces/DataLiquidityPoolStorageV1.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.24;
+
+import "./IDataLiquidityPool.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+/**
+ * @title Storage for DataLiquidityPool
+ * @notice For future upgrades, do not change DataLiquidityPoolStorageV1. Create a new
+ * contract which implements DataLiquidityPoolStorageV1
+ */
+abstract contract DataLiquidityPoolStorageV1 is IDataLiquidityPool {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
+    string public override name;
+    IERC20 public override token;
+    string public override masterKey;
+    uint256 public override maxNumberOfValidators;
+    uint256 public override minStakeAmount;
+    uint256 public override totalStaked;
+    uint256 public override totalValidatorsRewardAmount;
+    uint256 public override totalContributorsRewardAmount;
+    uint256 public override epochRewardAmount;
+    uint256 public override epochSize;
+    uint256 public override fileRewardFactor;
+    uint256 public override validationPeriod;
+    uint256 public override validatorScoreMinTrust;
+    uint256 public override validatorScoreKappa;
+    uint256 public override validatorScoreRho;
+    uint256 public override fileRewardDelay;
+    uint256 public override lastFinalizedFileId;
+
+    uint256 public override validatorsCount;
+    mapping(uint256 => address) internal _validators;
+    mapping(address => ValidatorInfo) internal _validatorsInfo;
+
+    uint256 public override activeValidatorsListsCount;
+    mapping(uint256 => EnumerableSet.AddressSet) internal _activeValidatorsLists;
+
+    mapping(uint256 => File) internal _files;
+    EnumerableSet.Bytes32Set internal _fileUrlHashes;
+
+    uint256 public override epochsCount;
+    mapping(uint256 => Epoch) internal _epochs;
+
+    uint256 public override contributorsCount;
+    mapping(uint256 => address) internal _contributors;
+    mapping(address => ContributorInfo) internal _contributorInfo;
+}
---
+++ deploy/dlp-upgrade.ts
@@ -0,0 +1,15 @@
+import { ethers, upgrades } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	const dlpDeploy = await upgrades.upgradeProxy(
+		process.env.DLP_CONTRACT_ADDRESS as string,
+		await ethers.getContractFactory("DataLiquidityPool"),
+	);
+
+	console.log("DataLiquidityPool upgraded");
+};
+
+export default func;
+func.tags = ["DLPUpgrade"];
